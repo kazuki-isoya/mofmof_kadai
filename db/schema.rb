d901fa209b1080a420b885e426cc9df10996d57d
moru more <moremoru.world21@gmail.com>
2020-02-07 14:47:27 +0900

    propertyをscaffoldで作成。カラムにnull: falseをマイグレーションファイルに追加してmigrateした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_07_054215) do
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "address", null: false
+    t.integer "age", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
