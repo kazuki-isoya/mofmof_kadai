761ccfb8a13a11e9f6abf6b37d243325c2eb9c0c
moru more <moremoru.world21@gmail.com>
2020-02-08 14:19:37 +0900

    propertyにバリデーションを追記

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
   has_many :near_stations
   accepts_nested_attributes_for :near_stations, allow_destroy: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :description, presence: true
 end
