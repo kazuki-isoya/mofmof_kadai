7677676e120ef5fe40539825e81751180034c6ba
moru more <moremoru.world21@gmail.com>
2020-02-09 22:41:14 +0900

    コメントの削除

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,46 +1,32 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
-    2.times { @property.near_stations.build }
 
   end
 
-  # GET /properties/1/edit
   def edit
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
-
-    respond_to do |format|
-      if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
-        format.json { render :show, status: :created, location: @property }
-      else
-        format.html { render :new }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.save
+      redirect_to properties_path,  notice:"登録しました！"
+    else
+      render :new
     end
+
+
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
@@ -53,8 +39,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -64,13 +48,11 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def property_params
-      params.require(:property).permit(:name, :price, :address, :age, :description, near_stations_attributes: [:route, :name, :walk_time])
-    end
+  def property_params
+    params.require(:property).permit(:name, :price, :address, :age, :description, near_stations_attributes: [:route, :name, :walk_time])
+  end
 end
